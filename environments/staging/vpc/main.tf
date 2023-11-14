2f1527a23ab7d2fa7eb4efccec322b3ece85b5de
vetuch.ta <vetuch.ta@placid.biz>
2023-11-14 15:02:12 +0700

    #5 Create VPC for staging env.

---
+++ environments/staging/vpc/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "remote" {
+    hostname =  "app.terraform.io"
+    organization = "beleiftfeks"
+
+    workspaces {
+        name = terraform-module-staging
+    }
+  }
+}
+
+module "vpc" {
+  source = "../../../modules/vpc"
+
+  env = "staging"
+  azs = ["ap-southeast-1a","ap-southeast-1b"]
+  private_subnets = ["10.35.0.0/19", "10.35.32.0/19"]
+  public_subnets = ["10.35.64.0/19", "10.35.96.0/19"]
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/cluster/dev-demo" = "owned"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+    "kubernetes.io/cluster/dev-demo" = "owned"
+  }  
+}
