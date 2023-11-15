caf9f9c07136e6ced4ae3633f6c4e738f39706a2
vetuch.ta <vetuch.ta@placid.biz>
2023-11-15 07:07:09 +0700

    Create tf file for eks

---
+++ environments/dev/eks/output.tf
@@ -0,0 +1,9 @@
+output "cluster_id" {
+  description = "The ID of the EKS cluster. Note: currently a value is returned only for local EKS clusters created on Outposts"
+  value       = module.eks.cluster_id
+}
+
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = module.eks.name
+}
