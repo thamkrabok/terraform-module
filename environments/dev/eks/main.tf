498c0728c0fb7f772c23d3d169da5238b58e1b25
thamkrabok <31384816+thamkrabok@users.noreply.github.com>
2023-11-15 07:31:10 +0700

    Update main.tf

--- environments/dev/eks/main.tf
+++ environments/dev/eks/main.tf
@@ -18,7 +18,7 @@ module "eks" {
     eks_version = "1.26"
     env         = "dev"
     eks_name    = "demo"
-    subnet      = ["subnet-0e00c5227cb7daaad","subnet-0d0d07080c562e554"]
+    subnet_ids      = ["subnet-0e00c5227cb7daaad","subnet-0d0d07080c562e554"]
 
     node_groups = {
         scaling_config   = {
