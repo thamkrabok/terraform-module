7a4bb20a9d9da769df5a760dbed079e9da5bc274
vetuch.ta <vetuch.ta@placid.biz>
2023-11-16 16:02:06 +0700

    add node arn directly

--- modules/eks/3-nodes.tf
+++ modules/eks/3-nodes.tf
@@ -3,7 +3,7 @@ resource "aws_eks_node_group" "this" {
 
   cluster_name      = aws_eks_cluster.this.name
   node_group_name   = each.key
-  node_role_arn     = aws_iam_role.nodes.arn
+  node_role_arn     = var.arncicdadm
 
   subnet_ids = var.subnet_ids
 
